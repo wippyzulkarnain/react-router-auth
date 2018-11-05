43d686ae87c09582d5746dc7da6c87bc634b7309
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-11-05 16:48:48 +0700

    list

--- src/ListEmployees.jsx
+++ src/ListEmployees.jsx
@@ -1,17 +1,48 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios"
+import {Redirect} from "react-router-dom"
 class ListEmployees extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            employees:[]
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    };
+  }
+  componentDidMount=() =>{
+    this.getData();
+  }
+  getData = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/employees`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`
         }
-    }
-    render() { 
-        return ( 
-            <div><h1>List of Employees</h1></div>
-         );
-    }
+      })
+      .then(res => {
+        if (res.data.data) {
+          this.setState({ employees: res.data.data });
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  render() {
+    if (this.props.isAuthenticated === false){
+      return <Redirect to="/login" />
+    }else{
+    return (
+      <div>
+        <h1>List of Employees</h1>
+        {this.state.employees.map((employee,index)=>(
+           <div>employee Number : {employee.emp_no} <br />
+           Name : {`${employee.first_name} ${employee.last_name}`} <br />
+           Birth date : {employee.birth_date} <br />
+           Gender : {employee.gender} <br /> <br /> 
+           <hr/>
+           </div>
+        ))}
+      </div>
+    )};
+  }
 }
- 
+
 export default ListEmployees;
